e1ea2b1128f9d4db33e06eae9e48e0496ef7b3bc
tchrlton <tchrlton1@gmail.com>
2025-05-22 17:23:28 -0700

    add extract markdown for links and images, plus tests

--- src/main.py
+++ src/main.py
@@ -1,10 +1,5 @@
-from textnode import TextNode, TextType
-from splitdelimiter import split_nodes_delimiter
-
 def main():
     print("hello world")
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
 
 if __name__ == "__main__":
     main()
---
+++ src/test_extractmarkdown.py
@@ -0,0 +1,30 @@
+import unittest
+from extractmarkdown import extract_markdown_images, extract_markdown_links
+
+class TestExtractMarkdown(unittest.TestCase):
+  def test_extract_markdown_images(self):
+    matches = extract_markdown_images(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+    )
+    self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+    
+  def test_extract_markdown_images_multiple(self):
+    matches = extract_markdown_images(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![image2](https://i.imgur.com/zjjcJKA.png)"
+    )
+    self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png"), ("image2", "https://i.imgur.com/zjjcJKA.png")], matches)
+    
+  def test_extract_markdown_links(self):
+    matches = extract_markdown_links(
+        "This is text with a [link](https://www.boot.dev)"
+    )
+    self.assertListEqual([("link", "https://www.boot.dev")], matches)
+    
+  def test_extract_markdown_links_multiple(self):
+    matches = extract_markdown_links(
+        "This is text with a [link](https://www.google.com) and another [link2](https://www.facebook.com)"
+    )
+    self.assertListEqual([("link", "https://www.google.com"), ("link2", "https://www.facebook.com")], matches)
+
+if __name__ == "__main__":
+  unittest.main()
