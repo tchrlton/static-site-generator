b46bce701303fb5c02560dfa2f083d13ac4c6f2a
tchrlton <tchrlton1@gmail.com>
2025-05-24 17:05:27 -0700

    add text to TextNodes logic and tests

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
+from texttotextnodes import text_to_textnodes
+
 def main():
-    print("hello world")
+    text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+    nodes = text_to_textnodes(text)
+    print(nodes)
 
 if __name__ == "__main__":
     main()
